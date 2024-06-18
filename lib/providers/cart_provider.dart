26f6acbea0206e47b9a725d0e377e829760fd7d9
Rohan Singh <rs7289579@gmail.com>
2024-06-18 10:34:41 +0530

    FUNC - Generated Cart Provider

--- lib/providers/cart_provider.dart
+++ lib/providers/cart_provider.dart
@@ -1,17 +1,13 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:riverpod_files/models/product.dart';
 
-class CartNotifier extends Notifier<Set<Product>> {
+part 'cart_provider.g.dart';
+
+@riverpod
+class CartNotifier extends _$CartNotifier {
   @override
   Set<Product> build() {
-    return const {
-      // initial products in cart
-      Product(
-          id: '2',
-          title: 'Karati Kit',
-          price: 34,
-          image: 'assets/products/karati.png'),
-    };
+    return const {};
   }
 
   // methods to update product
@@ -21,13 +17,9 @@ class CartNotifier extends Notifier<Set<Product>> {
     }
   }
 
-  void removeProduct (Product product){
-    if(state.contains(product)){
+  void removeProduct(Product product) {
+    if (state.contains(product)) {
       state = state.where((element) => element != product).toSet();
     }
   }
 }
-
-final cartNotifierProvider = NotifierProvider<CartNotifier, Set<Product>>(() {
-  return CartNotifier();
-});
